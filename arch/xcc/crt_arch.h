b58a9eedbf557bc3983deabd1298b5a419150e51
tyfkda <tyfkda@gmail.com>
2023-12-07 11:02:23 +0900

    _start実装追加

--- arch/xcc/crt_arch.h
+++ arch/xcc/crt_arch.h
@@ -1,1 +1,12 @@
-void _start(void) { }
+__asm(
+".text \n"
+".globl " START " \n"
+START ": \n"
+"	xor %rbp,%rbp \n"
+"	mov %rsp,%rdi \n"
+// ".weak _DYNAMIC \n"
+// ".hidden _DYNAMIC \n"
+// "	lea _DYNAMIC(%rip),%rsi \n"
+"	and $-16,%rsp \n"
+"	call " START "_c \n"
+);
